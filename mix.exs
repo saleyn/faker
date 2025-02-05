0001fc7e69b3c19dd4c925f5d034ace58613756b
Serge Aleynikov <saleyn@gmail.com>
2025-02-05 09:54:29 -0500

    Fix warnings

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Faker.Mixfile do
 
   defp deps do
     [
-      {:ex_doc, "== 0.36.1", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.37", only: :dev, runtime: false},
       {:earmark, "== 1.4.47", only: :dev, runtime: false},
       {:credo, "== 1.7.11", only: [:dev, :test], runtime: false},
       {:dialyxir, "== 1.4.5", only: [:dev], runtime: false},
